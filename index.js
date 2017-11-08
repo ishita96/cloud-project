f126e08eb19f036ea696293d1e1c5c4cec2cd27a
Ishita <ishita61996@gmail.com>
2017-11-08 13:01:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //index.js
-const http = require(&#39;http&#39;);
-const hostname = &#39;0.0.0.0&#39;;
+	const http = require('http');
+
+	const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
-const server = http.createServer((req, res) =&gt; {
-res.statusCode = 200;
-res.setHeader(&#39;Content-Type&#39;, &#39;text/plain&#39;);
-res.end(&#39;Hello World\n&#39;);
-});
-server.listen(port, hostname, () =&gt; {
-console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+	const server = http.createServer((req, res) => {
+  	res.statusCode = 200;
+  		res.setHeader('Content-Type', 'text/plain');
+  		res.end('Hello World\n');
+	});
+
+	server.listen(port, hostname, () => {
+  		console.log(`Server running at http://${hostname}:${port}/`);
+	});
